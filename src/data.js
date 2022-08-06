e5e71361805193dba1b32d8dee9cc98b9f87060a
Reena Chaudhari <107103119+rina996@users.noreply.github.com>
2022-08-06 12:43:48 +0530

    initial commit

---
+++ src/data.js
@@ -0,0 +1,66 @@
+const data = [
+  {
+    question: "In what kinds of societies are laws usually written down formally?",
+    incorrectAnswers: [
+      "small-scale societies",
+      "both of the above",
+      "neither of the above",
+    ],
+    correctAnswer: "large-scale societies",
+  },
+
+  {
+    question:
+      "Which of the following are important parts of social control in some small non-western societies?",
+    incorrectAnswers: [
+      "forced use of hallucinogenic drugs",
+      "both of the above",
+      "neither of the above",
+    ],
+    correctAnswer: "threat of witchcraft",
+  },
+  {
+    question: "Who decides whether a bill is a Money Bill or Not?",
+    incorrectAnswers: ["Minister of Parliamentary affairs", "Chairmen of Rajya Sabha", "President"],
+    correctAnswer: "Speaker of Lok Sabha",
+  },
+  {
+    question: "Which is the first state to introduce anti-lynching law?",
+    incorrectAnswers: ["Madhya Pradesh", "Rajasthan", "Orissa"],
+    correctAnswer: "Manipur",
+  },
+  {
+    question: "Under which article was the Finance Commission of India established?",
+    incorrectAnswers: [
+      "Article 260",
+      "Articla 317",
+      "Article 310",
+    ],
+    correctAnswer: "Article 280",
+  },
+  {
+    question: "Which State is the first to enact a law on contract farming?",
+    incorrectAnswers: [
+      "Adhra Pradesh",
+      "Uttar Pradesh",
+      "Telangana",
+    ],
+    correctAnswer: "Tamil Nadu",
+  },
+  {
+    question: "Who settles disputes in the election of President in India?",
+    incorrectAnswers: ["Attorney General of India", "Chairman of U.P.S.C", "Parliament"],
+    correctAnswer: "Supreme Court",
+  },
+  {
+    question: "Which one of the following is the guardian of Fundamental Rights?",
+    incorrectAnswers: [
+      "Legislature",
+      "Political parties",
+      "Executive",
+    ],
+    correctAnswer: "Judiciary",
+  },
+];
+
+export default data;
